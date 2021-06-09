67015afe3673f9a5e609a2fd4b8716e640c5d243
Julian <julian1986.producciones@gmail.com>
2021-06-09 19:21:29 -0300

    navBar + Limpieza

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-import './components/navBar/navBar.js'
 import './App.css';
 import {NavBar} from "./components/navBar/navBar";
 
 function App() {
   return (
       <NavBar />
-   /* <div className="App">
-
-      <header className="App-header">
-
-          <div className="logo grey">
-
-              <h2 className="logo__letter black">
-                  M
-              </h2>
-
-              <h3 className="logo__tittle black">
-                  Mueble Store
-              </h3>
-
-          </div>
-
-      </header>
-
-    </div>*/
   );
 }
 
--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -4,16 +4,13 @@ export const NavBar = () => {
         <nav className="nav">
 
             <div className="logo">
-
                 <div className="logo__letter">
                     M
                 </div>
                 <p className="logo__name">Mueble Store</p>
-
             </div>
 
             <ul className='menu'>
-
                 <li>
                     <a href="" className="menu__link">Productos</a>
                 </li>
@@ -25,9 +22,7 @@ export const NavBar = () => {
                 <li>
                     <a href="" className="menu__link">Contacto</a>
                 </li>
-
             </ul>
-
 
         </nav>
     )
